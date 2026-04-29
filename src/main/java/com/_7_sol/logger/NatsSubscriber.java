9bfc7a4bff4e3d3c210eef5db47c2d8474bcfb09
errol <errol.dennis@7-sol.com>
2026-04-28 22:18:51 -0400

    Remove unused SLF4J imports from `NatsSubscriber` to improve code cleanliness and dependency management.

--- src/main/java/com/_7_sol/logger/NatsSubscriber.java
+++ src/main/java/com/_7_sol/logger/NatsSubscriber.java
@@ -15,9 +15,6 @@ import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.stereotype.Service;
